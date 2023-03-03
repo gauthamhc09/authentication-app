117d9e4adc04744dbda8f7d20eb06ff3200efbb9
gauthamhc09 <gauthamhc09@gmail.com>
2023-03-03 12:23:24 +0530

    added nextauth-url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        NEXTAUTH_URL: 'https://authentication-app-swart.vercel.app/'
+      }
+    }
+  }
+}
