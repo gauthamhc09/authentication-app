0fed2598ae3889ebae152a38dc2d2284ecaa6fbd
gauthamhc09 <gauthamhc09@gmail.com>
2023-03-03 12:26:36 +0530

    removed the nextauth-url from next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        NEXTAUTH_URL: 'https://authentication-app-swart.vercel.app/'
-      }
-    }
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
+
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+// module.exports = (phase) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {
+//       env: {
+//         NEXTAUTH_URL: 'https://authentication-app-swart.vercel.app/'
+//       }
+//     }
+//   }
+// }
